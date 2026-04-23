d0166942e5bd2df529a73baf9a32c5cc9add3c2c
SifatNycto <sftmhd@gmail.com>
2026-04-24 00:38:12 +0600

    practiced python learning

--- Jarvis/main.py
+++ Jarvis/main.py
@@ -1,22 +1,20 @@
 from core.listen import record_audio
 from core.transcribe import transcribe_audio
+from core.brain import ask_llm
 
 
 def main():
     print("JARVIS is listening...")
 
     audio_file = record_audio(duration=5)
-
     if not audio_file:
-        print("⚠️ No usable audio")
         return
 
     text = transcribe_audio(audio_file)
+    if not text:
+        return
 
-    if text:
-        print("✅ Transcription complete")
-    else:
-        print("⚠️ Could not understand audio")
+    ask_llm(text)
 
 
 if __name__ == "__main__":
