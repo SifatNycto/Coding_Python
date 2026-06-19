1c4df6811f15dd2e5e7279788f8a39b16cce09a0
SifatNycto <sftmhd@gmail.com>
2026-06-19 23:20:49 +0600

    add cs50 flask with python practice

---
+++ cs50_py_flask/hello/app.py
@@ -0,0 +1,18 @@
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def index():
+#     return "hello, world"
+
+
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
